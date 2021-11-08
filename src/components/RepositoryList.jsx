181ae680a3068cf509b9752a081fe62c5c308b5d
Marcelo Tadini <tadini.marcelo53@gmail.com>
2021-11-08 15:56:07 -0300

    useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+//https://api.github/orgs/rocketseat/repos
 
 const repository = {
     name: 'unform',
@@ -10,6 +13,15 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
